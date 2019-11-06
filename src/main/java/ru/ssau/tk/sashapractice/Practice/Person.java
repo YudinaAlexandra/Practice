f7a785dbfa91d546dd9fbb4ce130701ecb440815
Yudina <ladyalexyudina@gmail.com>
2019-11-06 15:06:58 +0300

    создание класса Person

---
+++ src/main/java/ru/ssau/tk/sashapractice/Practice/Person.java
@@ -0,0 +1,50 @@
+package ru.ssau.tk.sashapractice.Practice;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int passportId;
+
+    public Person() {
+
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public Person(int passportId){
+        this.passportId = passportId;
+    }
+
+    public Person(String firstName, String lastName, int passportId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passportId = passportId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getPassportId() {
+        return passportId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPassportId(int passportId) {
+        this.passportId = passportId;
+    }
+
+}
