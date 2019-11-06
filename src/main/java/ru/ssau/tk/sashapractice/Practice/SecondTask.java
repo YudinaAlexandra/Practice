e93f0c73f2787265d2bddd107eeedffbb26a6c34
Yudina <ladyalexyudina@gmail.com>
2019-11-06 15:03:10 +0300

    task 1.2

---
+++ src/main/java/ru/ssau/tk/sashapractice/Practice/SecondTask.java
@@ -0,0 +1,15 @@
+package ru.ssau.tk.sashapractice.Practice;
+
+public class SecondTask {
+        static Person firstPerson = new Person();
+        static Person secondPerson = new Person("Helen", "Blooms");
+        static Person thirdPerson = new Person(548796);
+        static Person fourthPerson = new Person("Helen", "Blooms", 548796);
+    public static void main(String[] args)
+    {
+        System.out.println(firstPerson.getFirstName() + ' ' + firstPerson.getLastName() + ", passport " + firstPerson.getPassportId());
+        System.out.println(secondPerson.getFirstName() + ' ' + secondPerson.getLastName() + ", passport " + secondPerson.getPassportId());
+        System.out.println(thirdPerson.getFirstName() + ' ' + thirdPerson.getLastName() + ", passport " + thirdPerson.getPassportId());
+        System.out.println(fourthPerson.getFirstName() + ' ' + fourthPerson.getLastName() + ", passport " + fourthPerson.getPassportId());
+    }
+}
