867325efff64b4b2fc315eb7286fe130778b2cf6
Yudina <ladyalexyudina@gmail.com>
2019-11-08 13:22:45 +0300

    Point task 1.3

---
+++ src/main/java/ru/ssau/tk/sashapractice/Practice/Point.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.sashapractice.Practice;
+
+public class Point {
+    public final double x;
+    public final double y;
+    public final double z;
+    public Point(double x, double y, double z){
+        this.x=x;
+        this.y=y;
+        this.z=z;
+    }
+}
---
+++ src/main/java/ru/ssau/tk/sashapractice/Practice/ThirdTask.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.sashapractice.Practice;
+
+public class ThirdTask {
+    public static void main (String[] args) {
+        Point point1 = new Point(1, 2, 3);
+        Point point2 = new Point(4, 5, 6);
+        Point point3 = new Point(7, 8, 9);
+        System.out.println( point1.x);
+        System.out.println(point2.y);
+        System.out.println(point3.z);
+    }
+}
